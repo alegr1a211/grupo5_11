d28b3c682d290275bfaf827644447aedeb8e2a8a
erick <ef3929450@gmail.com>
2019-11-25 11:24:42 -0400

    cambios

--- archivo_unico.ts
+++ archivo_unico.ts
@@ -2,3 +2,8 @@ let numero1 = 80;
 let numero2 = 2;
 let resultado = numero1 / numero2;
 console.log(resultado);
+
+function restar(valor1: number, valor2: number): number{
+    return valor1-valor2;
+}
+console.log(restar(20, 10))
