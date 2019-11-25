910cb7388e6cf6dad4e065a21cebeb6c9367a763
Rodrigo Zenteno <rodrigo.zenteno35@gmail.com>
2019-11-25 11:17:57 -0400

    --

--- archivo_unico.ts
+++ archivo_unico.ts
@@ -2,3 +2,10 @@ let numero1 = 80;
 let numero2 = 2;
 let resultado = numero1 / numero2;
 console.log(resultado);
+
+var multiplicacion = 10;
+var multiplicador = 100;
+var cant = 0;
+var i = 0;
+cant = multiplicacion * multiplicador;
+console.log('la Multiplicacion es : ',cant);
