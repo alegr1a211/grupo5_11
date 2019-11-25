5dfd7050db8ba2c5176aa81fec683d26375e51e1
Brian Coria <brian_coria@outlook.com>
2019-11-25 16:22:32 -0400

    prueba develop

--- archivo_unico.ts
+++ archivo_unico.ts
@@ -1,11 +1,11 @@
 let numero1 = 80;
 let numero2 = 2;
 let resultado = numero1 / numero2;
-console.log("el resultado de esta division es", resultado);
+console.log("El resultado de esta division es: ", resultado);
 
 var multiplicacion = 10;
 var multiplicador = 100;
 var cant = 0;
 var i = 0;
 cant = multiplicacion * multiplicador;
-console.log('la Multiplicacion es : ',cant);
+console.log('La Multiplicacion es : ',cant);
